625855b06ec62777b2952136ff9b0c553a14523b
p0937507934 <p0937507934@gmail.com>
2021-09-07 23:32:50 +0800

    modify basename

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./App.module.scss";
-import { Link, Route, withRouter, useLocation } from "react-router-dom";
+import { Link, Route, withRouter, useLocation,BrowserRouter } from "react-router-dom";
 import { HomePage } from "./HomePage/index";
 import { WorkPage } from "./WorkPage/index";
 import { WorkPageDetail } from "./WorkPageDetail/index";
@@ -35,9 +35,13 @@ function App() {
       </header>
 
       <section className={styles.section}>
+        <BrowserRouter basename="/resume">
         <Route path="/" exact component={HomePage}></Route>
         <Route path="/works" exact component={WorkPage}></Route>
         {/* <Route path="/works/:id" exact component={WorkPageDetail}></Route> */}
+        </BrowserRouter>
+
+        
       </section>
 
       <footer className={styles.footer}>
