b8d81fbef1f7e0e93b46b7d0e3f6f383298c918d
p0937507934 <p0937507934@gmail.com>
2021-09-07 23:33:26 +0800

    Updates

---
+++ precache-manifest.1c595bcb57fb2175d09c52ff33e13370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9889dd14b4831292ac773e3f996e2e7",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "c82aede6d3ddf11d261d",
+    "url": "/resume/static/css/main.5f86cd60.chunk.css"
+  },
+  {
+    "revision": "72f981bfa61ee1c1a1dd",
+    "url": "/resume/static/js/2.3ba6dccc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.3ba6dccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82aede6d3ddf11d261d",
+    "url": "/resume/static/js/main.ccc946ef.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  },
+  {
+    "revision": "55a32c96860f579f382748a638e849b9",
+    "url": "/resume/static/media/avatar.55a32c96.jpg"
+  }
+]);
